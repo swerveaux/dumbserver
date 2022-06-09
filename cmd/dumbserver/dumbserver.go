79688d156c0aa2c2db09d438b3d7bff57976c83f
Brian Nilsen <swervo@gmail.com>
2022-06-09 11:04:55 -0700

    Added option to show request body in logs

--- cmd/dumbserver/dumbserver.go
+++ cmd/dumbserver/dumbserver.go
@@ -3,20 +3,33 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+
+	"github.com/spf13/pflag"
 )
 
 type LogMessage struct {
 	Path    string      `json:"path"`
+	Body    string      `json:"body"`
 	Headers http.Header `json:"headers""`
 }
 
 func main() {
+	var showBody bool
+	pflag.BoolVar(&showBody, "show-body", false, "Set to true if you want the request bodies to be included in log output")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		m := LogMessage{
 			Path:    r.URL.Path,
 			Headers: r.Header,
+		}
+		if showBody {
+			b, err := io.ReadAll(r.Body)
+			if err != nil {
+				log.Print("{\"error\":\"Unable to read request body.\"}")
+			}
+			m.Body = string(b)
 		}
 		err := json.NewEncoder(log.Writer()).Encode(m)
 		if err != nil {
