45c22efd633252876bb6249ea109e793bc391ab3
Brian Nilsen <swervo@gmail.com>
2022-06-13 16:35:19 -0700

    HTTP status code can be changed from 200 via command line

--- cmd/dumbserver/dumbserver.go
+++ cmd/dumbserver/dumbserver.go
@@ -20,9 +20,11 @@ type LogMessage struct {
 func main() {
 	var showBody, logToStderr bool
 	port := 8080
+	responseCode := 200
 	pflag.BoolVar(&showBody, "show-body", false, "Set if you want the request bodies to be included in log output")
 	pflag.BoolVar(&logToStderr, "log-to-stderr", false, "Set if you want logging to go to stderr. It's usually easier to keep it unset if you want to do something like pipe to jq.")
 	pflag.IntVar(&port, "port", 8080, "Port to listen on")
+	pflag.IntVar(&responseCode, "response-code", 200, "HTTP status code for response, default is 200")
 	pflag.Parse()
 	if !logToStderr {
 		log.SetOutput(os.Stdout)
@@ -47,6 +49,7 @@ func main() {
 			fmt.Fprint(w, "Somehow failed writing logs")
 			return
 		}
+		w.WriteHeader(responseCode)
 		fmt.Fprint(w, "{\"result\": \"ok\"")
 	})
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
